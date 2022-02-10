0c301bc11615635e9f95e84f311093fd1a3e021f
Robin Phillips <robinlmp@mac.com>
2022-02-10 13:28:16 +0000

    More tidying up

--- Challenge5/DetailViewController.swift
+++ Challenge5/DetailViewController.swift
@@ -16,25 +16,10 @@ class DetailViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Register cell classes
-        
         title = country?.name
-        
-        // Do any additional setup after loading the view.
+
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using [segue destinationViewController].
-     // Pass the selected object to the new view controller.
-     }
-     */
     
     // MARK: UICollectionViewDataSource
     
